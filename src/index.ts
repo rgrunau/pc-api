23938e65ea5477666eea83c076082a0d781dd3bd
Robert Grunau <89792978+rgrunau@users.noreply.github.com>
2024-01-20 08:38:01 -0800

    it all connects

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,44 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const prisma = new PrismaClient();
-const app = express();
 
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
-app.post("/user/create", async (req, res) => {
+app.post("/api/user/create", async (req, res) => {
   console.log(`creating new user`);
-  const result = await prisma.user.create({
-    data: {
-      ...req.body,
-    },
-  });
-  res.json(result);
+  console.log(req.body);
+  try {
+    const result = await prisma.user.create({
+      data: {
+        ...req.body,
+      },
+    });
+    console.log("user created");
+    if (!result) {
+      res.json({
+        message: "error",
+        data: "user not created",
+      });
+    }
+    res.json({
+      message: "success",
+      data: result,
+    });
+    console.log(`created new user with id: ${result.id}`);
+  } catch (err) {
+    res.json({
+      message: "error",
+      data: err,
+    });
+  }
 });
 
 app.listen(3000, () => {
